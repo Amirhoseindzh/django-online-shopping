645edd363d068345c06217869ddc5e57bff5a216
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2024-10-17 23:29:53 +0330

    check users is_active after create them.

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,20 @@
+# app/tests.py
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+
+    def test_create_regular_user_is_inactive(self):
+        """
+        Test that regular users are created with is_active=False.
+        """
+        user = User.objects.create_user(username='alireza', email='regularuser1@example.com', password='password123')
+        self.assertFalse(user.is_active, msg="Regular users should be inactive by default")
+
+    def test_create_superuser_is_active(self):
+        """
+        Test that superusers are created with is_active=True.
+        """
+        superuser = User.objects.create_superuser(username='_admin',email='admin1@example.com', password='admin123')
+        self.assertTrue(superuser.is_active, msg="Superusers should be active by default")
