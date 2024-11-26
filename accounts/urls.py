4bb0fd0428d4089c7e19dfc483d2eb94884cdc83
Amirhoseindzh <mrahd66@gmail.com>
2024-11-26 17:58:44 +0330

    added address_list url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 
 from .views import UserProfile
 from .views import CustomLoginView
+from .views import address_list
+
 
 urlpatterns = [
     path('accounts/login/', CustomLoginView.as_view(), name='account_login'),
@@ -12,4 +14,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
+    # addresses urls
+    path("addresses/", address_list, name="address_list"),
 ]
